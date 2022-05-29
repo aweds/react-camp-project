aa81419852ec14d6525a208e2dd34865f6b13ec5
osmankirci <osman_kirci@windowslive.com>
2022-05-29 21:32:48 +0300

    11. ders sonu

--- src/layouts/CartSummary.jsx
+++ src/layouts/CartSummary.jsx
@@ -1,9 +1,10 @@
+
 import React from 'react'
-import {  Dropdown } from 'semantic-ui-react'
 
 export default function CartSummary() {
     return (
-        <div>
+        <div> 
+            
         </div>
     )
 }
--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Button, Container, Dropdown,Menu } from 'semantic-ui-react'
+import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
 
 export default function Navi() {
     return (
         <div>
-            <Menu inverted fixed >
+            <Menu inverted fixed="top" >
                 <Container>
                     <Menu.Item name='home' />
+
                     <Menu.Item name='messages' />
 
                     <Menu.Menu position='right'>
-
                         <Dropdown item text='Language'>
                             <Dropdown.Menu>
                                 <Dropdown.Item>English</Dropdown.Item>
@@ -18,11 +18,11 @@ export default function Navi() {
                                 <Dropdown.Item>Spanish</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
-
                         <Menu.Item>
                             <Button primary>Sign Up</Button>
                         </Menu.Item>
                     </Menu.Menu>
+                    
                 </Container>
 
             </Menu>
